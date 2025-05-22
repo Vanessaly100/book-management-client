6b6f2139ee6953c811866f06fdea1bfe43d74d29
Vanessaly100 <vanessalydia360@gmail.com>
2025-05-22 18:01:49 +0100

    update

--- src/pages/public/Contact/Maps.jsx
+++ src/pages/public/Contact/Maps.jsx
@@ -1,4 +1,3 @@
-// import React from 'react'
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
@@ -10,26 +9,26 @@ const center = {
   lat: 37.819928,
   lng: -122.478255,
 };
+
 const Maps = () => {
+  const googleMapsApiKey = import.meta.env.VITE_googleMapsApiKey; 
+
   return (
     <div>
-        <div className="p-4 bg-darkMainBg text-white">
-                <p className="mb-4 xl:text-3xl text-2xl">Find us at our location below:</p>
-                <LoadScript
-                  googleMapsApiKey="AIzaSyBEIjUO7JZowrCCUFsFYKbujJLYxmJP-1Y
-        "
-                >
-                  <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={10}
-                  >
-                    <Marker position={center} />
-                  </GoogleMap>
-                </LoadScript>
-              </div>
+      <div className="p-4 bg-darkMainBg text-white">
+        <p className="mb-4 xl:text-3xl text-2xl">Find us at our location below:</p>
+        <LoadScript googleMapsApiKey={googleMapsApiKey}> 
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+          >
+            <Marker position={center} />
+          </GoogleMap>
+        </LoadScript>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Maps
+export default Maps;
