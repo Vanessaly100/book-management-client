19d9230f4f9248463a06e0322d31bd670ed94e96
Vanessaly100 <vanessalydia360@gmail.com>
2025-08-07 04:10:42 +0100

    Updated project with latest changes

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,39 +1,22 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-
+console.log("API baseURL:", import.meta.env.VITE_PUBLIC_API_URL);
 const api = axios.create({
   baseURL: import.meta.env.VITE_PUBLIC_API_URL,
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  timeout: 10000,
 });
 
 
 api.interceptors.request.use(
   (config) => {
     const accessToken = Cookies.get("accessToken");
-
-    // Don't attach Authorization header to login or refresh-token routes
-    const skipAuth =
-      config.url.includes("/auth/login") ||
-      config.url.includes("/auth/refresh-token");
-
-    if (accessToken && !skipAuth) {
+    if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
-
-    console.log("Making request to:", config.baseURL + config.url);
     return config;
   },
-  (error) => {
-    console.error("Request error:", error);
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
-
-
 
 api.interceptors.response.use(
   (response) => response,
@@ -69,4 +52,5 @@ api.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
 export default api;
