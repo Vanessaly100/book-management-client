96791b232f811ab7421c00e6c2a48ad124849676
Vanessaly100 <vanessalydia360@gmail.com>
2025-08-07 17:05:45 +0100

    Updated project with latest changes

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -17,39 +17,35 @@ api.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
+// api.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
 
-    if (
-      error.response?.status === 401 &&
-      !originalRequest._retry &&
-      !originalRequest.url.includes("/auth/refresh-token")
-    ) {
-      originalRequest._retry = true;
+//     if (
+//       error.response?.status === 401 &&
+//       !originalRequest._retry &&
+//       !originalRequest.url.includes("/auth/refresh-token")
+//     ) {
+//       originalRequest._retry = true;
+//       try {
+//         const res = await api.post("/auth/refresh-token");
+//         const newToken = res.data.accessToken;
 
-      try {
-        const res = await api.post("/auth/refresh-token");
-        const newAccessToken = res.data.accessToken;
+//         if (newToken) {
+//           Cookies.set("accessToken", newToken, { expires: 7 });
+//           originalRequest.headers.Authorization = `Bearer ${newToken}`;
+//           return api(originalRequest);
+//         }
+//       } catch (err) {
+//         Cookies.remove("accessToken");
+//         Cookies.remove("user");
+//         window.location.href = "/login";
+//       }
+//     }
 
-        if (newAccessToken) {
-          Cookies.set("accessToken", newAccessToken);
-          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-          return api(originalRequest);
-        }
-      } catch (err) {
-        console.error("Token refresh failed:", err);
-        Cookies.remove("accessToken");
-        Cookies.remove("user");
-
-        if (window.location.pathname !== "/login") {
-          window.location.href = "/login";
-        }
-      }
-    }
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 export default api;
